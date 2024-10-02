10f451da0b7d32ae6122ec0439a7e089fc882ac0
Horea Oros <horea.oros@gmail.com>
2024-10-02 13:57:23 +0300

    Add project files.

---
+++ 1002/1002/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+for (int i = 1; i <= 1000; i++)
+    f(i);
+
+void f(int n)
+{
+    Console.Write($"{n} ");
+
+    while ( n > 1 )
+    {
+        if (n % 2 == 0)
+            n = n / 2;
+        else
+            n = 3 * n + 1;
+        Console.Write($"{n} ");
+    }
+
+    Console.WriteLine();
+}
